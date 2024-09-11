83551b614f04e6e943918f4f625b037f0dbb9fbe
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-09-10 21:29:42 -0700

    avance practica2, juego carro

---
+++ Practicas/Practica_2/Car.h
@@ -0,0 +1,141 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+class Car
+{
+private:
+    sf::Vector2f position;
+    sf::Vector2f direction;
+    float angle;
+    float speed;
+    float fuel;
+    bool isOn;
+    bool isTurningLeft;
+    bool isTurningRight;
+    sf::Sprite sprite;
+    sf::Texture textureOff, textureOn, textureLeft, textureRight, textureBrake;
+
+public:
+    Car(sf::Vector2f position, sf::Vector2f direction, float angle, float speed, float fuel,
+        bool isOn, bool isTurningLeft, bool isTurningRight)
+    {
+        this->position = position;
+        this->direction = direction;
+        this->angle = angle;
+        this->speed = speed;
+        this->fuel = fuel;
+        this->isOn = isOn;
+        this->isTurningLeft = isTurningLeft;
+        this->isTurningRight = isTurningRight;
+        this->textureBrake = textureBrake;
+
+        // Cargar texturas
+        textureOff.loadFromFile("assets/apagado.png");
+        textureOn.loadFromFile("assets/encendido.png");
+        textureLeft.loadFromFile("assets/giro_izquierda.png");
+        textureRight.loadFromFile("assets/giro_derecha.png");
+        textureBrake.loadFromFile("assets/freno.png");
+        sprite.setTexture(textureOff); // Por defecto el carro está apagado
+        sprite.setPosition(position);
+    }
+
+    void TurnOn()
+    {
+        isOn = true;
+        sprite.setTexture(textureOn);
+    }
+    void TurnOff()
+    {
+        isOn = false;
+        sprite.setTexture(textureOff);
+    }
+    void Accelerate(float deltaTime)
+    {
+        if (isOn && fuel > 0)
+        {
+            speed += 500 * deltaTime; // Incrementa la velocidad
+            fuel -= 35 * deltaTime;    // Decrementa gasolina
+        }
+    }
+
+    void Brake(float deltaTime)
+    {
+        if (isOn)
+        {
+            if (speed > 0)
+            {
+                speed = std::max(0.0f, speed - 250 * deltaTime);
+                sprite.setTexture(textureBrake);
+            }
+        }
+    }
+
+    void TurnLeft(float deltaTime)
+    {
+        if (speed > 0)
+        {
+            angle -= 250 * deltaTime; // Reduce el giro
+            direction = Rotar(direction, -100 * deltaTime);
+            sprite.setTexture(textureLeft);
+        }
+    }
+    void TurnRight(float deltaTime)
+    {
+        if (speed > 0)
+        {
+            angle += 250 * deltaTime; // Incrementa el giro
+            direction = Rotar(direction, 100 * deltaTime);
+            sprite.setTexture(textureRight);
+        }
+    }
+    void StopTurning()
+    {
+        if ((isTurningLeft || isTurningRight) && speed > 0)
+        {
+            sprite.setTexture(textureOn);
+            isTurningLeft = isTurningRight = false;
+        }
+    }
+    void PumpGas(float amount) { fuel = std::min(100.0f, fuel + amount); }
+
+    void Update(float deltaTime, sf::RenderWindow &window)
+    {
+        if (isOn && fuel > 0)
+        {
+            // Mover el carro
+            position += direction * speed * deltaTime;
+
+            // Verificar límites de la ventana
+            if (position.x < 0)
+                position.x = 0;
+            if (position.y < 0)
+                position.y = 0;
+            if (position.x + sprite.getGlobalBounds().width > window.getSize().x)
+                position.x = window.getSize().x - sprite.getGlobalBounds().width;
+            if (position.y + sprite.getGlobalBounds().height > window.getSize().y)
+                position.y = window.getSize().y - sprite.getGlobalBounds().height;
+
+            sprite.setPosition(position);
+        }
+    }
+
+    void Display(sf::RenderWindow &window)
+    {
+        window.draw(sprite);
+    }
+
+    sf::Vector2f Rotar(sf::Vector2f v, float deg)
+    {
+        float rad = deg * 3.1416 / 180;
+        float sn = sin(rad);
+        float cs = cos(rad);
+        return sf::Vector2f(v.x * cs - v.y * sn, v.x * sn + v.y * cs);
+    }
+
+    // Getter y setter
+    float getFuel() { return fuel; }
+
+    sf::Vector2f getPosition() { return position; }
+
+    float getSpeed() { return speed; }
+};
--- Practicas/Practica_2/main.cpp
+++ Practicas/Practica_2/main.cpp
@@ -1,5 +1,164 @@
-#include <SFML/Graphics.hpp>
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+#define FUEL_MAX 100
+
+#include "Car.h"
+#include <iostream>
+#include <iomanip> // para setPrecision
+#include <sstream> // para ostringstream
 
 int main()
 {
+    // Crear ventana
+    sf::RenderWindow window(sf::VideoMode(SCR_WIDTH, SCR_HEIGHT), "Practica 2 - LPOO");
+
+    // Cargar la fuente
+    sf::Font font;
+    if (!font.loadFromFile("assets/Roboto-Regular.ttf"))
+    {
+        return -1;
+    }
+
+    /******** Creacion de la calle *******/
+    // Crear la calle (rectángulo gris)
+    sf::RectangleShape street(sf::Vector2f(800, 200)); // Ancho 800, Alto 200
+    street.setFillColor(sf::Color(100, 100, 100));     // Color gris
+    street.setPosition(0, 200);                        // Posición centrada verticalmente en la pantalla
+
+    // Crear las líneas de carril (rectángulos pequeños blancos)
+    sf::RectangleShape laneLine(sf::Vector2f(60, 5)); // Ancho 60, Alto 5
+    laneLine.setFillColor(sf::Color::White);          // Color blanco
+
+    // Variables para las líneas de carril
+    float lineSpacing = 100; // Espaciado entre las líneas
+    float lineOffset = 50;   // Offset para centrar la línea de carril en la calle
+
+    /*********************/
+
+    // Crear el texto para mostrar la cantidad de gasolina
+    sf::Text fuelText;
+    fuelText.setFont(font);                  // Asignar la fuente
+    fuelText.setCharacterSize(28);           // Tamaño del texto
+    fuelText.setFillColor(sf::Color::White); // Color del texto
+    fuelText.setPosition(10, 10);            // Posición del texto en la pantalla
+
+    // Crear el texto para mostrar la velocidad actual
+    sf::Text speedText;
+    speedText.setFont(font);                  // Asignar la fuente
+    speedText.setCharacterSize(28);           // Tamaño del texto
+    speedText.setFillColor(sf::Color::White); // Color del texto
+    speedText.setPosition(10, 50);            // Posición del texto en la pantalla
+
+    // Crear el texto para mostrar las instrucciones de las teclas
+    sf::Text instructionsText;
+    instructionsText.setFont(font);                     // Asignar la fuente
+    instructionsText.setCharacterSize(20);              // Tamaño del texto
+    instructionsText.setFillColor(sf::Color::White);    // Color del texto
+    instructionsText.setPosition(10, SCR_HEIGHT - 150); // Posición del texto en la pantalla
+    instructionsText.setString("E: Encender\nA: Apagar\nUp: Acelerar\nDown: Frenar\nLeft: Izquierda\nRight: Derecha");
+
+    // Reloj para calcular deltaTime
+    sf::Clock clock;
+
+    // Gasolina y velocidad iniciales
+    float fuel = FUEL_MAX;
+    float speed = 0;
+
+    // Crear el objeto Car
+    Car car(sf::Vector2f(SCR_WIDTH / 2 - 32, SCR_HEIGHT - 120), sf::Vector2f(0, -1), 0, 0, FUEL_MAX, false, false, false);
+
+    // Bucle principal del juego
+    while (window.isOpen())
+    {
+        sf::Time deltaTime = clock.restart();
+        float dt = deltaTime.asSeconds();
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            // Manejo de eventos del teclado
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+            {
+                car.TurnOn();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                car.TurnOff();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                car.Accelerate(dt);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                car.Brake(dt);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                car.TurnRight(dt);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                car.TurnLeft(dt);
+            }
+
+            // Dejar de girar y frenar
+            if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                car.StopTurning();
+            }
+        }
+
+        // Actualizar el estado del carro
+        car.Update(dt, window);
+
+        // Actualizar el texto de la gasolina
+        if (fuel <= FUEL_MAX && fuel >= 75)
+        {
+            fuelText.setFillColor(sf::Color::Green);
+        }
+        else if (fuel < FUEL_MAX * 0.75 && fuel >= FUEL_MAX * 0.5)
+        {
+            fuelText.setFillColor(sf::Color::Yellow);
+        }
+        else if (fuel < FUEL_MAX * 0.5 && fuel >= FUEL_MAX * 0.25)
+        {
+            fuelText.setFillColor(sf::Color::Red);
+        }
+        else
+        {
+            fuelText.setFillColor(sf::Color::Black);
+        }
+
+        // Actualizar el texto de la gasolina
+        fuel = car.getFuel();
+        fuelText.setString("Gasolina: " + std::to_string(static_cast<int>(fuel)));
+
+        // Actualizar el texto de la velocidad
+        speed = car.getSpeed();
+        // stream para formatear velocidad
+        std::ostringstream stream;
+        stream << std::fixed << std::setprecision(2) << speed;
+        speedText.setString("Velocidad: " + stream.str() + " km/h");
+
+        // Dibujar todo en la ventana
+        window.clear();
+        window.draw(street);
+        window.draw(fuelText);
+        window.draw(speedText);
+        window.draw(instructionsText);
+
+        // Dibujar el carro
+        car.Display(window);
+
+        // Mostrar lo que se ha dibujado
+        window.display();
+    }
+
+    return 0;
 }
